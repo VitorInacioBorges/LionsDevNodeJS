ab506636cc28f75ef375af320e6a75b128d8344e
Vitor Inacio <vitorinacioborges@gmail.com>
2025-07-10 11:51:55 -0300

    refractor altered password.js

--- .js/password.js
+++ .js/password.js
@@ -1,29 +1,87 @@
-const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?/:;.,<>-=+_()*&¨%$#@!"^~}]{[';
+const characters = 'abcdefghijklmnopqrstuvwxyz';
+const capsCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '1234567890';
+const especialCharacters = '!?/:;.,<>-=+_()*&¨%$#@!"^~}]{[';
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+let includeCaps = true;
+let includeLetters = true;
+let includeNumbers = true;
+let includeEspecial = true;
+
+function updatePrompt() {
+    console.clear();
+    let promptMessage = "Write the type of characters you want to include in your generated password:\n";
+    promptMessage += `1 - Capslock letters -------- [${includeCaps ? 'ACTIVATED' : 'DEACTIVATED'}]\n`;
+    promptMessage += `2 - Letters ----------------- [${includeLetters ? 'ACTIVATED' : 'DEACTIVATED'}]\n`;
+    promptMessage += `3 - Numbers ----------------- [${includeNumbers ? 'ACTIVATED' : 'DEACTIVATED'}]\n`;
+    promptMessage += `4 - Especials Characters ---- [${includeEspecial ? 'ACTIVATED' : 'DEACTIVATED'}]\n`;
+    promptMessage += "Enter numbers to toggle options (e.g., '13' to activate Caps and Numbers), then 'P' to proceed or '0' to close! --> ";
+    console.log(includeCaps);
+    rl.setPrompt(promptMessage);
+    rl.prompt();
+}
 
 console.clear();
-rl.setPrompt("Write the length of the password you want to generate:\n0 to close! --> ");
-rl.prompt();
+updatePrompt();
 rl.on('line', (input) => {
     if(isNaN(input)){
         console.log("Invalid number!\n");
         rl.prompt();
-    } else if(input == 0){
-        console.log("CLOSING...")
-        rl.close();
+    } else if (input == 'P'){
+        rl.question("\nEnter the length you want for your password: ", (lengthInput) => {
+            if(isNaN(lengthInput) || lengthInput < 1){
+                console.log("Invalid length! Try again!");
+                updatePrompt();
+            }
+
+            let password = '';
+            let availableCharacters = '';
+            if (includeLetters) availableCharacters += characters;
+            if (includeCaps) availableCharacters += capsCharacters;
+            if (includeNumbers) availableCharacters += numbers;
+            if (includeEspecial) availableCharacters += especialCharacters;
+
+            if (availableCharacters.length === 0) {
+                console.log("No characters selected, it is not possible to generate a password!");
+                rl.prompt();
+                return;
+            }
+            console.log(`\nYour generated password is: ${password}\n`);
+            updatePrompt();
+        })
     } else {
-        const length = parseInt(input);
-        let password = '';
-        for(let i=0; i<length; i++){
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            password += characters.charAt(randomIndex);
+        let validToggle = false;
+        switch(input){
+                case 0:
+                    rl.close();
+                    break;
+                case 1:
+                    includeCaps = false;
+                    validToggle = true;
+                    updatePrompt()
+                    break;
+                case 2:
+                    includeLetters = !includeLetters;
+                    validToggle = true;
+                    break;
+                case 3:
+                    includeNumbers = !includeNumbers;
+                    validToggle = true;
+                    break;
+                case 4:
+                    includeEspecial = !includeNumbers;
+                    validToggle = true;
+                    break;
+                default:
+                    break;
+            }
+        if (!validToggle && input !== 'P' && input !== '0') {
+            console.log("Invalid input! Enter numbers to toggle options, 'G' to generate or '0' to close the program.");
         }
-        console.clear();
-        console.log(`Generated Password: ${password}`);
-        rl.prompt();
+        updatePrompt();
     }
 })
