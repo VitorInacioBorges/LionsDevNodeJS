4e9a7534fa9babc3338149d5dda908ab72069d5c
Vitor Inacio <vitorinacioborges@gmail.com>
2025-07-16 08:27:11 -0300

    refractor added population.js

---
+++ src/jsBasics/population.js
@@ -0,0 +1,15 @@
+const percentA = 3 / 100;
+const percentB = 1.5 / 100;
+let A = 80000;
+let B = 200000;
+let year = 0;
+
+for(let i = 0; A < B; i++){
+    A += A * percentA;
+    B += B * percentB;
+    console.log(`A population is: ${A}`);
+    console.log(`B population is ${B}\n`);
+    year++;
+}
+
+console.log(`It took ${year} years for A to surpass B!`);
