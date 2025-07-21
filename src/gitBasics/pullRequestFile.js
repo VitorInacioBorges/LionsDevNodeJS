906accfd7906b87ba0a6f0d7e37d61770c1162fa
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-07-21 09:15:51 -0300

    PR test commit

--- src/gitBasics/pullRequestFile.js
+++ src/gitBasics/pullRequestFile.js
@@ -1,2 +1,5 @@
-let i = Math.floor(Math.random() * 100) + 1; // 1 excluded and 0 included
+let i = Math.floor(Math.random() * 200) + 1; // 1 excluded and 0 included
 console.log(i); // 1-100
+
+// This file is gonna be used to make a pull request from a branch called 'temp'
+// If everything goes as expected the altered part is gonna be the range of Math.random from 1-100 to 1-200
