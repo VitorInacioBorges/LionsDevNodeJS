dd6fe8122ff725d2451008999aa1d30e76e1c9e6
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-08-07 10:37:24 -0300

    refractor altered caesarCipher.js

---
+++ src/jsProjects/singleFiles/caeserCipher.js
@@ -0,0 +1,25 @@
+function caesarCipher(x, y){
+    if(x < 'a' || x > 'z'){
+        return x;
+    }
+
+    const firstCharCode = 'a'.charCodeAt();
+    const charCode = x.charCodeAt();
+    const newX = firstCharCode + (charCode - firstCharCode + y) % 26;
+
+    return String.fromCharCode(newX);
+}
+
+function encryptSentenceWithCaeserCipher(sentence, number){
+    let encryptedSentence = '';
+    sentence = sentence.toLowerCase();
+
+    for(let i=0; i<sentence.length; i++){
+        let newS = caesarCipher(sentence[i], number);
+        encryptedSentence += newS;
+    }
+
+    return encryptedSentence;
+}
+
+console.log(encryptSentenceWithCaeserCipher('abc def ghi', 1));
